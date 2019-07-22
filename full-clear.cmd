4c3539e24db2f5e412f52b756653ffb0d559255e
Andrey Novakovskiy <andrey@itlab.com.ua>
2019-07-22 23:32:42 +0300

    Changed start and stop files

--- full-clear.cmd
+++ full-clear.cmd
@@ -1,5 +1,6 @@
-docker-compose stop
 FOR /f "tokens=*" %%i IN ('docker container ls -aq') DO docker container stop %%i
 FOR /f "tokens=*" %%i IN ('docker network ls') DO docker network rm %%i
 docker network prune -f
 docker system prune -f
+docker volume prune -f
+docker image prune -a -f
